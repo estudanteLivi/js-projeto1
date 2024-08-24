851202e28755f30a9c3327e6bcaa76fe384dfe8d
Lívia Silva <00001125575190sp@al.educacao.sp.gov.br>
2024-08-24 13:45:57 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,59 @@
+function draw() {
+    fill(red);
+    circle(posicaoHorizontal, posicaoVertical, 50);
+  
+    if(mouseIsPressed){
+        cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+    }
+}function draw() {
+
+    fill(red);
+    circle(posicaoHorizontal, posicaoVertical, 50);
+    
+    posicaoHorizontal+= random(0,3);
+    
+    if(mouseIsPressed){
+        cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+    }
+}function draw() {
+
+  
+  fill(green);
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  
+  posicaoHorizontal+= random(0,3);
+  posicaoVertical+= random(-3,3);
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+  
+}function setup() {
+
+  createCanvas(400, 400);
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 0;
+  posicaoVertical = random(height);
+}let cor;
+
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+let posicaoHorizontal2; // x
+let posicaoVertical2; // y
+function draw() {
+  
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  circle(posicaoHorizontal2, posicaoVertical2, 50);
+  
+  posicaoHorizontal+= random(0,3);
+  posicaoVertical+= random(-3,3);
+  posicaoHorizontal2+= random(0,3);
+  posicaoVertical2+= random(-3,3);
+  
+  if(mouseIsPressed){
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
